6989a01de2f4d86d0160b15cf0a09ef0c9f3db56
kimmari10 <kimmari10@naver.com>
2020-05-09 17:15:59 +0900

    initial commit

---
+++ src/main/java/com/bot/chat/ChatBotReplierApplication.java
@@ -0,0 +1,13 @@
+package com.bot.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotReplierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatBotReplierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bot/chat/ChatBotReplierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bot.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotReplierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
