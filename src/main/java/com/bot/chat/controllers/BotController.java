127104608aece5666a05c0a52c932e0b871c0a0d
kimmari10 <kimmari10@naver.com>
2020-05-13 18:39:57 +0900

    api 파라미터 dto로 처리

--- src/main/java/com/bot/chat/controllers/BotController.java
+++ src/main/java/com/bot/chat/controllers/BotController.java
@@ -1,10 +1,10 @@
 package com.bot.chat.controllers;
 
+import com.bot.chat.dto.RequestDto;
 import com.bot.chat.dto.ResponseDto;
 import com.bot.chat.service.CommandService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,10 +14,19 @@ public class BotController {
     private final String COMMAND_SIGNAL = "!";
     private final CommandService commandService;
 
-    @GetMapping("/api/bot/{msg}")
-    public ResponseDto proc(@PathVariable String msg) {
+    @GetMapping("/api/bot/reply")
+    public ResponseDto proc(RequestDto requestDto) {
+        String msg = requestDto.getMsg();
 
-        //1. !로시작하는 커맨드인지 확인
+        //1. Sender(Member) 저장
+
+        //2. Message 저장
+
+        //3. Room 저장
+
+        //4. 메세지 처리
+
+        //4-1. !로시작하는 커맨드인지 확인
         if(msg.startsWith(COMMAND_SIGNAL)) {
 
             if(msg.endsWith("날씨")) {
--- src/main/java/com/bot/chat/dto/RequestDto.java
+++ src/main/java/com/bot/chat/dto/RequestDto.java
@@ -1,4 +1,19 @@
 package com.bot.chat.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@Builder
+@ToString
 public class RequestDto {
+    private String room;
+    private String msg;
+    private String sender;
+    private boolean isGroupChat;
+
 }
+
