f6f59cbfa35a50ca467ba8ea198e186c28a3efdf
sinaeYu <anne.sinae@gmail.com>
2020-05-28 02:12:59 +0900

    명령어리스트 4차 업로드 - 카드뷰 포지셔닝 수정, 키워드뷰 기본세팅

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -36,59 +36,74 @@ function chkSystemWords(target) {
 
 /* view type menu for command list : 명령어리스트 뷰타입관련 메뉴 */
 function viewCmdMenu(target) {
-    var $list = $(".command-list");
-    var gnrlData = $list.find("li").not(".system");
-    var sysData = $list.find("li").filter(".system"), sysIdx = [];
+    var list = ".command-list";
+    var gnrlData = $(list).find("li").not(".system");
+    var sysData = $(list).find("li").filter(".system"), sysIdx = [];
 
-    $list.find("li").each(function(i) {
+    $(list).find("li").each(function(i) {
         if($(this).hasClass("system")) sysIdx.push(i);
     });
 
    $(target).find("a").on("click", function(e) {
         var liName = $(this).parent().attr("class");
         if(liName == "cmd_all") {
-            $list.html(gnrlData);
+            $(list).html(gnrlData);
             $.each(sysIdx, function(i, val) {
-                $list.find("li").eq(val).before(sysData[i]);
-            })
+                $(list).find("li").eq(val).before(sysData[i]);
+            });
+            masonry(list);
         }
         if(liName == "cmd_general") {
-            $list.empty().html(gnrlData);
+            $(list).empty().html(gnrlData);
+            masonry(list);
         }
         if(liName == "cmd_system") {
-            $list.empty().html(sysData);
+            $(list).empty().html(sysData);
+            masonry(list);
         }
-
+        if(liName == "vt_card") {
+            $(this).addClass("on");
+            $(".vt_kwd a").removeClass("on");
+            $(list).removeClass("kwd");
+        }
+        if(liName == "vt_kwd") {
+            $(this).addClass("on");
+            $(".vt_card a").removeClass("on");
+            $(list).addClass("kwd");
+        }
         e.preventDefault();
    });
 }
 
-/* align to masonry type : 벽돌형식 포지셔닝 */
+/* set masonry : 문서로딩시 벽돌형식 포지셔닝 세팅 */
 function setMasonry(target) {
-	masonry();
+	masonry(target);
 	$(window).on("resize", function() {
         if(this.resizeTo) clearTimeout(this.resizeTo);
         this.resizeTo = setTimeout(function() {
-            masonry();
+            masonry(target);
         }, 300);
     });
-	function masonry() {
-        $(target).children().css("position", "static");
-        var colNum = parseInt($(target).width()/$(target).children().outerWidth());
-        var arrLeft = [], arrTop = [];
-        $(target).children().each(function(childIndex) {
-            var thisH = $(this).outerHeight() + parseInt($(this).css("marginBottom")) + parseInt($(this).css("marginTop"));
-            if(childIndex < colNum) {
-                arrLeft.push($(this).position().left);
-                arrTop.push(thisH);
-                $(this).stop().animate({"top" : 0, "left" : arrLeft[childIndex]}, 400);
-            } else {
-                var minTop = Math.min.apply(Math, arrTop);
-                var minIndex = arrTop.indexOf(minTop);
-                $(this).stop().animate({"top" : minTop, "left" : arrLeft[minIndex]}, 400);
-                arrTop[minIndex] = thisH + minTop;
-            }
-        });
-        $(target).css({"position":"relative", "height": Math.max.apply(Math, arrTop) + parseInt($(target).css("paddingBottom"))}).children().css({"position":"absolute"});
-	}
+
+}
+
+/* align to masonry type : 벽돌형식 포지셔닝 */
+function masonry(target) {
+    $(target).children().css("position", "static");
+    var colNum = parseInt($(target).width()/$(target).children().outerWidth());
+    var arrLeft = [], arrTop = [];
+    $(target).children().each(function(childIndex) {
+        var thisH = $(this).outerHeight() + parseInt($(this).css("marginBottom")) + parseInt($(this).css("marginTop"));
+        if(childIndex < colNum) {
+            arrLeft.push($(this).position().left);
+            arrTop.push(thisH);
+            $(this).stop().animate({"top" : 0, "left" : arrLeft[childIndex]}, 400);
+        } else {
+            var minTop = Math.min.apply(Math, arrTop);
+            var minIndex = arrTop.indexOf(minTop);
+            $(this).stop().animate({"top" : minTop, "left" : arrLeft[minIndex]}, 400);
+            arrTop[minIndex] = thisH + minTop;
+        }
+    });
+    $(target).css({"position":"relative", "height": Math.max.apply(Math, arrTop) + parseInt($(target).css("paddingBottom"))}).children().css({"position":"absolute"});
 }
